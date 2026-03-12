3865ba1ea8d2a7b0c2c05b1b1df34a2b67ce331d
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-03-12 05:31:17 -0700

    Some pictures and cache

--- snakelet/adapter.js
+++ snakelet/adapter.js
@@ -2,9 +2,21 @@ module.exports = {
 
     started: false,
 
-    start() {
+    client(v) {
+        if(!v) return require("../cache.js").client;
+        else require("../cache.js").client = v;
+    },
+    async start() {
 
         this.started = true;
+        console.log("Starting : Snake Bot");
+
+        setTimeout(this.client({ready:true}),3000);
+
+        await new Promise(resolve => setInterval(() => {
+            console.log(this.client());
+            if(this.client()?.ready) resolve();
+        },1000));
 
     }
 
