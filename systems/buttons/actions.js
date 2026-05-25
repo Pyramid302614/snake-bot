688e9d1de1fb3cd0b11632302c382ba5746963a1
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-05-24 17:36:17 -0700

    Changed  some stuff about action 1092

--- systems/buttons/actions.js
+++ systems/buttons/actions.js
@@ -5,81 +5,83 @@ const { MessageFlags, ContainerBuilder, TextDisplayBuilder, ButtonStyle } = requ
 module.exports = {
     "1092": async (interaction) => {
         
-        if(!u.sbdb.getGuildProperty(interaction.guild.id,"minigame.id")) {
-            interaction.update({});
-            return;
-        }
-        const users = u.sbdb.getGuildProperty(interaction.guild.id,"minigame.users") ?? {};
-        users[interaction.user.id] = "unknown";
         try {
-            await interaction.launchActivity({
-                "application_id": u.adapter.chip?u.adapter.config30.beetroot_client_id:u.adapter.config30.snakebot_client_id
-            });
-            u.adapter.timeouts.push(setTimeout(() => {
-                if(u.sbdb.getGuildProperty(interaction.guild.id,"minigame.users."+interaction.user.id) == "unknown") u.sbdb.updateGuildProperty(interaction.guild.id,"minigame.users."+interaction.user.id,"unfulfilled");
-            },15_000));
-        } catch(e) {
-            console.log(e);
-            interaction.update({});
-            return;
-            var msg = "An error has occurred while trying to open that activity.";
-            if(u.adapter.config30.chosen_ones.includes(interaction.user.id)) msg += "\n```"+e.stack+"```";
-            switch(e.code) {
-                case 50230: msg = "You cannot open activities in your current client."; break;
+            if(!u.sbdb.getGuildProperty(interaction.guild.id,"minigame.id")) {
+                interaction.message.delete();
+                return;
             }
-            await interaction.user.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle(u.errTitles.newTitle("generalPack"))
-                        .setDescription(msg)
-                        .setColor([255,0,0])
-                ],
-                flags: [MessageFlags.Ephemeral]
-            });
-            return;
-        }
-        u.sbdb.updateGuildProperty(interaction.guild.id,"minigame.users",users);
-        
-        if(!u.sbdb.getGuildProperty(interaction.guild.id,"DSAMs."+interaction.user.id+".mobileActivityConnectionLatencyWarning")) {
-            interaction.followUp({
-                components: [
-                    new ContainerBuilder()
-                        .addTextDisplayComponents(
-                            new TextDisplayBuilder()
-                                .setContent(
-`### Warning!!!!!! ⚠️⚠️🚨🚨🚨
-Opening the catch activity **on mobile** is
-**fully supported**, but "warming it up" takes
-abnormally long. I do not know why this is like this,
-but just know **if the screen is black**, give it
-a **good 10-20 seconds** to load up.
+            const users = u.sbdb.getGuildProperty(interaction.guild.id,"minigame.users") ?? {};
+            users[interaction.user.id] = "unknown";
+            try {
+                await interaction.launchActivity({
+                    "application_id": u.adapter.chip?u.adapter.config30.beetroot_client_id:u.adapter.config30.snakebot_client_id
+                });
+                u.adapter.timeouts.push(setTimeout(() => {
+                    if(u.sbdb.getGuildProperty(interaction.guild.id,"minigame.users."+interaction.user.id) == "unknown") u.sbdb.updateGuildProperty(interaction.guild.id,"minigame.users."+interaction.user.id,"unfulfilled");
+                },15_000));
+            } catch(e) {
+                interaction.update({});
+                return;
+                var msg = "An error has occurred while trying to open that activity.";
+                if(u.adapter.config30.chosen_ones.includes(interaction.user.id)) msg += "\n```"+e.stack+"```";
+                switch(e.code) {
+                    case 50230: msg = "You cannot open activities in your current client."; break;
+                }
+                await interaction.user.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(u.errTitles.newTitle("generalPack"))
+                            .setDescription(msg)
+                            .setColor([255,0,0])
+                    ],
+                    flags: [MessageFlags.Ephemeral]
+                });
+                return;
+            }
+            u.sbdb.updateGuildProperty(interaction.guild.id,"minigame.users",users);
+            
+            if(!u.sbdb.getGuildProperty(interaction.guild.id,"DSAMs."+interaction.user.id+".mobileActivityConnectionLatencyWarning")) {
+                interaction.followUp({
+                    components: [
+                        new ContainerBuilder()
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder()
+                                    .setContent(
+    `### Warning!!!!!! ⚠️⚠️🚨🚨🚨
+    Opening the catch activity **on mobile** is
+    **fully supported**, but "warming it up" takes
+    abnormally long. I do not know why this is like this,
+    but just know **if the screen is black**, give it
+    a **good 10-20 seconds** to load up.
 
-Luckily, mobile minigames are designed to be **easier**
-and **more leniant**, so you don't have to worry
-about being behind if you primarily use mobile.`
-                                )
-                        )
-                        .addActionRowComponents(
-                            new ActionRowBuilder()
-                                .addComponents(
-                                    u.msgelem.messageElement(
-                                        new ButtonBuilder()
-                                            .setLabel("Don't show this again please")
-                                            .setStyle(ButtonStyle.Secondary),
-                                        async (del,b_interaction,d) => {
-                                            del();
-                                            await b_interaction.update({});
-                                            b_interaction.deleteReply("@original");
-                                            u.sbdb.updateGuildProperty(interaction.guild.id,"DSAMs."+interaction.user.id+".mobileActivityConnectionLatencyWarning",true);
-                                        },
-                                        [interaction.user.id]
-                                    ).data
-                                )
-                        )
-                        .setAccentColor(u.color.rgb("#ff5100"))
-                ],
-                flags: [MessageFlags.Ephemeral,MessageFlags.IsComponentsV2]
-            });
+    Luckily, mobile minigames are designed to be **easier**
+    and **more leniant**, so you don't have to worry
+    about being behind if you primarily use mobile.`
+                                    )
+                            )
+                            .addActionRowComponents(
+                                new ActionRowBuilder()
+                                    .addComponents(
+                                        u.msgelem.messageElement(
+                                            new ButtonBuilder()
+                                                .setLabel("Don't show this again please")
+                                                .setStyle(ButtonStyle.Secondary),
+                                            async (del,b_interaction,d) => {
+                                                del();
+                                                await b_interaction.update({});
+                                                b_interaction.deleteReply("@original");
+                                                u.sbdb.updateGuildProperty(interaction.guild.id,"DSAMs."+interaction.user.id+".mobileActivityConnectionLatencyWarning",true);
+                                            },
+                                            [interaction.user.id]
+                                        ).data
+                                    )
+                            )
+                            .setAccentColor(u.color.rgb("#ff5100"))
+                    ],
+                    flags: [MessageFlags.Ephemeral,MessageFlags.IsComponentsV2]
+                });
+            }
+        } catch(ignored) {
         }
 
     },
